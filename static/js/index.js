de54584bc0fe6b036af48b8eac90803a23f2f2e4
pranjaldatta <pranjaldatta99@gmail.com>
2020-03-25 17:34:59 +0530

    added index code

--- static/js/index.js
+++ static/js/index.js
@@ -9,13 +9,23 @@ function handleUpload(e) {
 
 //document.querySelector('.file-select').addEventListener('change', handleFileChange);
 //document.querySelector('.file-submit').addEventListener('click', handleUpload);
-var formdata = new FormData()
-$('.file-select').change(function(e) {
-    console.log('file changed!')
-    console.log(e.target.files[0])
-    formdata.append('imagefile', $('.file-select')[0].files[0])
-})
 
+
+function handleChange(input) {
+    console.log("handleChange fired")
+    imgbox = $('#input-image')
+    console.log(input)
+    if(input.files && input.files[0]) {
+        let reader = new FileReader()
+        reader.onload = function(e) {
+            imgbox.attr('src', e.target.result)
+            imgbox.height(256)
+            imgbox.width(256)
+        }
+        reader.readAsDataURL(input.files[0])
+    }
+}
+/*
 url = "http://localhost:5000/convert"
 $('.file-submit').click(function() { 
     console.log("clicked")
@@ -32,4 +42,33 @@ $('.file-submit').click(function() {
 
     })
   
+})*/
+
+const url = "http://localhost:5000/convert"
+$('.file-submit').click(function() {
+    console.log("clicked!")
+    imgbox = $('#input-image')
+    outputbox = $('#output-image')
+    input = $('.file-select')[0]
+    if(input.files && input.files[0]) {
+        let formData = new FormData()
+        formData.append('image', input.files[0])
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: formData,
+            cache: false,
+            processData: false,
+            contentType: false,
+            error: function(error) {
+                console.log("ERROR", error)
+                console.log(error.getAllResponseHeaders())
+            },
+            success: function(data){
+                fake_img = data.split('\'')[3]
+                outputbox.attr('src' , 'data:image/jpeg;base64,'+fake_img)
+            }
+
+        })
+    }
 })
